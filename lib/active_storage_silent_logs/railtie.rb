01c130d07b3ae01b84f152b3c0758f90ec27389a
Yasuda <yasuda@ex-corp.co.jp>
2022-09-12 15:45:28 +0900

    Fix deprecation warning
    ActiveStorage::BaseController autoloads on boot and on every reload.

--- lib/active_storage_silent_logs/railtie.rb
+++ lib/active_storage_silent_logs/railtie.rb
@@ -3,7 +3,9 @@ module ActiveStorageSilentLogs
     initializer "active_storage_silent_logs" do
       Rails.application.config.middleware.insert_before Rack::Sendfile, ActiveStorageSilentLogs::SilentLogger
 
-      ActiveStorage::BaseController.send :include, ActiveStorageSilentLogs::Controller
+      Rails.application.reloader.to_prepare do
+        ActiveStorage::BaseController.send :include, ActiveStorageSilentLogs::Controller
+      end
 
       Rails.application.config.filter_parameters += [:content_type, :disposition, :encoded_key, :signed_blob_id, :variation_key]
     end
